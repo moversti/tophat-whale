7863cf8ccd3a51096eb37a06509b813497c32378
Mikko Översti <moversti92@gmail.com>
2019-05-06 13:28:14 +0300

    Revert "gh- pages fixes"
    This reverts commit da67d8628757f50b19eabec17d5a0fca53e3cd07.

--- src/components/Product.js
+++ src/components/Product.js
@@ -7,7 +7,7 @@ export default function Product(props){
     return (
             <div className='col-lg-3 col-md-4 col-sm-6 my-2'>
                 <div className='card mx-2 h-100'>
-                    <Link to={'/tophat-whale/details/'+iidee}><img src={props.product.img} className="card-img-top mt-2" alt={props.product.title}></img></Link>
+                    <Link to={'/details/'+iidee}><img src={props.product.img} className="card-img-top mt-2" alt={props.product.title}></img></Link>
                     <div className="card-body d-flex flex-column">
                         <h5 className="card-title mt-auto">{props.product.title}</h5>
                         <p className="card-text">{props.product.price}EUR</p>
