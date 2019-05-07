e3ae7e8521b907bdc8856426808e3d22ef8fe9b9
Mikko Översti <moversti92@gmail.com>
2019-05-07 10:53:07 +0300

    contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,33 @@
 import React from 'react';
 
 export default function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div className="container d-flex flex-column">
+      <h4 className="my-3">Ota yhteyttä</h4>
+      <form>
+        <div className="form-group">
+          <label>Nimi</label>
+          <input className="form-control" type="text" placeholder="Nimesi" />
+        </div>
+        <div className="form-group">
+          <label>Email</label>
+          <input
+            className="form-control"
+            type="email"
+            placeholder="sinun@email.fi"
+          />
+        </div>
+        <div className="form-group">
+          <label>Palaute</label>
+          <textarea className="form-control" />
+        </div>
+        <input
+          type="submit"
+          value="Lähetä"
+          className="btn btn-primary"
+          placeholder="Mitä asiaa?"
+        />
+      </form>
+    </div>
+  );
 }
