e8de9cec2c2c2cc77a583e72b29969320510a2ec
Mikko Översti <moversti92@gmail.com>
2019-05-07 10:57:51 +0300

    made emoji accessible, fix warning

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,9 @@ export default function About() {
         Parhaat hatut ja hattutarvikkeet AAA luottoluokituksen omaavalta
         yritykseltä.
       </h4>
-      <h5>Huom: Ei oikeasti &#x1F61C;</h5>
+      <h5>
+        Huom: Ei oikeasti <span role="img" aria-label="just kidding" />&#x1F61C;
+      </h5>
     </div>
   );
 }
