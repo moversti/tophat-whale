d7df67fc783380547e9c10fe5512d789a1299548
Mikko Översti <moversti92@gmail.com>
2019-05-13 14:27:07 +0300

    can input numbers in cart items

--- src/components/Cartitem.js
+++ src/components/Cartitem.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { ProductConsumer } from '../context';
 
+function changeQuantity(e, setQuantity) {
+  const tar = e.target;
+  setQuantity(tar.value);
+}
+
 export default function Cartitem({ product }) {
+  const [quantity, setQuantity] = useState(product.count);
+  useEffect(() => {
+    product.count = quantity;
+    product.total = product.count * product.price;
+  });
   return (
     <tr>
       <td>
@@ -16,19 +26,26 @@ export default function Cartitem({ product }) {
               <React.Fragment>
                 <button
                   className="btn btn-danger"
-                  disabled={product.count < 1 ? 'true' : false}
+                  disabled={product.count < 1 ? true : false}
                   onClick={() => {
+                    setQuantity(Number.parseInt(quantity) - 1);
                     value.minusOne(product.id);
                   }}
                 >
                   -
                 </button>
-                <button disabled className="btn mx-2 border border-dark">
-                  {product.count}
-                </button>
+                <input
+                  type="number"
+                  value={quantity}
+                  onChange={e => {
+                    changeQuantity(e, setQuantity);
+                  }}
+                  className="btn mx-2 border border-dark"
+                />
                 <button
                   className="btn btn-success"
                   onClick={() => {
+                    setQuantity(Number.parseInt(quantity) + 1);
                     value.plusOne(product.id);
                   }}
                 >
