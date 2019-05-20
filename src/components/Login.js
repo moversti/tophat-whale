dabfdeed037d4204ef9619f265ea73f5c29ca143
Mikko Översti <moversti92@gmail.com>
2019-05-20 09:57:29 +0300

    user creation function

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import { Formik, Form, ErrorMessage } from 'formik';
+import firebase from '../firebase';
+
+function createUser(email, password) {
+  return firebase.auth().createUserWithEmailAndPassword(email, password);
+}
 
 export default function Login() {
   return <div>Login</div>;
